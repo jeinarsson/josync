4636aa7cd0ded553d87ff318315a701504c44f32
Joel Goop <joel.goop@chalmers.se>
2014-03-31 19:00:29 +0200

    Tried different output reading which finally seems to work.

--- rsync_backup.py
+++ rsync_backup.py
@@ -11,39 +11,11 @@ from Queue import Queue, Empty
 
 ON_POSIX = 'posix' in sys.builtin_module_names
 
-def log_worker(stdout,stderr):
-    '''Read output in a thread.'''
-    while True:
-        # output = non_block_read(stdout).strip()
-        output = stdout.readline().strip()
-        errors = stderr.readline().strip()
-        if output != '' or errors != '':
-            print "OUT: "+output
-            print "ERR: "+errors
-
-
-
-
-
-def enqueue_output(out, queue):
+def print_output(out):
     for line in iter(out.readline, b''):
-        queue.put(line)
+        print line.strip()
     out.close()
 
-p = Popen(['myprogram.exe'], stdout=PIPE, bufsize=1, close_fds=ON_POSIX)
-q = Queue()
-t = Thread(target=enqueue_output, args=(p.stdout, q))
-t.daemon = True # thread dies with the program
-t.start()
-
-# ... do other things here
-
-# read line without blocking
-try:  line = q.get_nowait() # or q.get(timeout=.1)
-except Empty:
-    print('no output yet')
-else: # got line
-    # ... do something with line
 
 # def non_block_read(output):
 #     '''Reading non-blocking'''
@@ -72,16 +44,20 @@ def main():
             raise Exception('Source directories must not end with /')
         print "Backing up {} to {}.".format(s,target)
         cygwin_bash = '{}/bash.exe'.format(config['cygwin_bindir'])
-        rsync_call = [cygwin_bash,'--login','--verbose','-c','rsync']+rsync_options+[s,target]
-        rsync_process = sp.Popen(rsync_call,stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
+        rsync_call = [cygwin_bash,'--login','-c']+[' '.join(['rsync']+rsync_options+[s,target])]
+        print "rsync call is:\n"+' '.join(rsync_call)
+        rsync_process = sp.Popen(rsync_call,stdout=subprocess.PIPE,stderr=subprocess.PIPE,bufsize=1,close_fds=ON_POSIX)
 
-
-        thread = Thread(target=log_worker, args=[rsync_process.stdout,rsync_process.stderr], name="rsync_output_thread")
-        thread.daemon = True
-        thread.start()
+        thread_err = Thread(target=print_output, args=(rsync_process.stderr,), name="rsync_error_thread")
+        thread_err.daemon = True
+        thread_err.start()
+        thread_out = Thread(target=print_output, args=(rsync_process.stdout,), name="rsync_out_thread")
+        thread_out.daemon = True
+        thread_out.start()
 
         rsync_process.wait()
-        thread.join(timeout=1)
+        thread_err.join()
+        thread_out.join()
 
 if __name__ == '__main__':
     main()
