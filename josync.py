b71c51a3b1ef0f494cc2e2d9daca230c03b90cf9
Joel Goop <joel.goop@chalmers.se>
2014-04-11 16:15:26 +0200

    Removed rsync_backup and pasted relevant parts into new main file josync.py.

--- josync.py
+++ josync.py
@@ -4,14 +4,17 @@ import jobs
 def main():
 
 	print "Helo. This is josync v. 42."
-	
-	# parse command line args
+
+	# TODO parse command line args
+
+    # parse global settings file
+    utils.read_config(default_cfg='default.josync-config',user_cfg='user.josync-config')
 
 	# parse job file
+    sj = SyncJob('syncjob-example.josync-job')
 
-	# parse global settings file
 
-	# execute job
+	# TODO execute job
 
 
 	print "good bye"
