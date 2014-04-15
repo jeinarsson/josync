3c19c723a9da9637b3a5a994e09b5721499a658d
Joel Goop <joelgoop@gmail.com>
2014-04-15 22:24:18 +0200

    Added AdditiveJob
    Set rsync options to give permissions and added / to end of source paths
    not to create the source dir in target.

--- jobs.py
+++ jobs.py
@@ -12,6 +12,7 @@ class Job(object):
     """Parent class for backup jobs."""
     def __init__(self,params):
         super(Job, self).__init__()
+        self.rsync_options = []
 
         logger.info("Initializing Job")
         self.params = params
@@ -46,35 +47,56 @@ class Job(object):
             else:
                 self.sources[drive] = [path]
 
+        if 'excludes' not in self.params:
+            self.params['excludes'] = []
+
+
 
     def run(self):
         raise NotImplementedError("Run method of job was not implemented.")
 
+    def prepare_source(self,drive_root,source_path):
+        """Prepare source for rsync call.
 
-class SyncJob(Job):
-    """Simple backup syncing multiple sources to a target directory."""
+        :param drive_root: Temp dir for source drive shadow copy.
+        :type drive_root: str
+        :param source_path: Path relative to drive base.
+        :type source_path: str
+        :returns: str -- Source path ready for rsync.
+        """
+        return utils.get_cygwin_path("{}{}".format(drive_root,source_path))
+
+    def prepare_target(self,target):
+        """Prepare target for rsync call.
+
+        :param target: Target path.
+        :type target: str
+        :returns: str -- Target path ready for rsync."""
+        return utils.get_cygwin_path(target)
+
+
+class BaseSyncJob(Job):
+    """Base class for sync-type jobs."""
     def __init__(self,params):
-        super(SyncJob, self).__init__(params)
-        logger.info("Initializing SyncJob.")
+        super(BaseSyncJob, self).__init__(params)
+        self.rsync_options += ['-vzh','--chmod=ugo=rwX']
 
     def run(self):
         """Run rsync to sync one or more sources with one target directory."""
-        target = self.target
-        rsync_options = ['-avzh','--chmod=ugo=rwX','--delete']
         for excl in self.params['excludes']:
-            rsync_options += ["--exclude",excl]
+            self.rsync_options += ["--exclude",excl]
 
         for drive,paths in self.sources.items():
             logger.info("Backing up sources on {}".format(drive))
             with utils.volume_shadow(drive) as drive_root:
                 for source_path in paths:
-                    logger.info("Backing up {}{} to {}".format(drive,source_path,target))
+                    logger.info("Backing up {}{} to {}".format(drive,source_path,self.target))
                     logger.debug("Drive root is found at {} and source path is {}.".format(drive_root,source_path))
 
-                    cygsource = utils.get_cygwin_path("{}{}".format(drive_root,source_path))
-                    cygtarget = utils.get_cygwin_path(target)
+                    cygsource = self.prepare_source(drive,source_path)
+                    cygtarget = self.prepare_target(self.target)
 
-                    rsync_call = [utils.config['rsync_bin']]+rsync_options+[cygsource,cygtarget]
+                    rsync_call = [utils.config['rsync_bin']]+self.rsync_options+[cygsource,cygtarget]
                     logger.debug("rsync call is {}".format(' '.join(rsync_call)))
                     # Run rsync
                     # TODO capture and maybe parse output
@@ -87,9 +109,39 @@ class SyncJob(Job):
                     else:
                         logger.info("rsync finished successfully.")
 
+
+class SyncJob(BaseSyncJob):
+    """Simple backup syncing multiple sources to a target directory with full tree structure."""
+    def __init__(self,params):
+        super(SyncJob, self).__init__(params)
+        logger.info("Initializing SyncJob.")
+
+        # Delete option to keep up-to-date with sources
+        # Relative option to create directory tree at target
+        self.rsync_options += ['--archive','--delete','--relative']
+
+    def prepare_source(self,drive,source_path):
+        # Insert /./ in source path to create tree at target relative to after /cygdrive/
+        return super(SyncJob, self).prepare_source(drive,source_path).replace('/cygdrive/','/cygdrive/./')
+
+
+class AdditiveJob(BaseSyncJob):
+    """Updating target with new files from sources."""
+    def __init__(self,params):
+        super(AdditiveJob, self).__init__(params)
+        logger.info("Initializing AdditiveJob.")
+
+        self.rsync_options += ['--recursive','--perms']
+
+    def prepare_source(self,drive,source_path):
+        # Add / at the end to start in folder
+        return super(AdditiveJob, self).prepare_source(drive,source_path)+'/'
+
+
 # enumerate all possible job types and their constructors
 job_types = {
-    'sync': SyncJob
+    'sync': SyncJob,
+    'add': AdditiveJob
 }
 
 
